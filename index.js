dce649ef4d340edd328997a58a52f7875818f38d
Tilak Sharma <tilak.sharma.ai@gmail.com>
2025-06-28 09:11:16 +0000

    Add getHumanChoice() function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     switch (choice){
@@ -12,4 +15,20 @@ function getComputerChoice() {
     }
 }
 
-// console.log(getComputerChoice());
+
+
+function getHumanChoice() {
+    const message = "1. Rock\n2. Paper\n3. Scissors\n\n";
+    let choice = Number(prompt(`${message}Select one of the above: `));
+    switch (choice){
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+    }
+}
+
